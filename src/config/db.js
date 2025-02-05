6a828247c50673845afc9e33af57d19a7e96e777
san287 <sk151027.sa@gmail.com>
2025-02-05 15:59:33 +0530

    updated logs in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const baseurl=process.env.MONGODBURL ||'127.0.0.1:27017';
+console.log("MongoUrl comming from .env file: "+baseurl);
+
 export const connectToDB=async()=>{
     try {
+        console.log("MongoUrl before mongoose connection: "+baseurl);
         await mongoose.connect(`mongodb://${baseurl}/userservice`);
         console.log('Connected to MongoDB');
     } catch (error) {
-        console.log('Error connecting to MongoDB:',error);        
+        console.log('Error connecting to MongoDB:',error.message);        
     }    
 }
